accc108c2aaadc11c0abf4f2a977464f088d926f
Hyerin <116632714+choihyerln@users.noreply.github.com>
2023-08-16 15:28:59 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,10 @@
+a,b,c = map(int, input().split())
+
+def dac(a,b,c):
+    if b == 1:
+        return a%c
+    elif b % 2 == 0:
+        return (dac(a,b//2,c)**2)%c
+    elif b % 2 == 1:
+        return (dac(a, b//2, c)**2)*a%c
+print(dac(a,b,c))
