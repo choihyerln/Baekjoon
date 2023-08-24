1b233038c4a27f0c554e3ccb1626177b97a6f715
Hyerin <116632714+choihyerln@users.noreply.github.com>
2023-08-24 10:38:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 35728 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().split())
+maxi = 100001
+visited = [0] * maxi
+
+def bfs(x):
+    queue = deque([x])
+    while queue:
+        x = queue.popleft()
+
+        if x == k:
+            return visited[x]
+        
+        for next_x in (x-1, x+1, 2*x):
+            if  0 <= next_x < maxi and not visited[next_x]:
+                visited[next_x] = visited[x] + 1
+                queue.append(next_x)
+
+                if next_x == k:
+                    return visited[next_x]
+print(bfs(n))
