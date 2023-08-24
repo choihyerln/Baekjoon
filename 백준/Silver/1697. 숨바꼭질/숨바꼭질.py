fe0e6863d452099095379a0e3b5a66d707c159ba
Hyerin <116632714+choihyerln@users.noreply.github.com>
2023-08-24 10:44:44 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 35336 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -9,15 +9,13 @@ def bfs(x):
     queue = deque([x])
     while queue:
         x = queue.popleft()
-
+        
         if x == k:
             return visited[x]
-        
+
         for next_x in (x-1, x+1, 2*x):
             if  0 <= next_x < maxi and not visited[next_x]:
                 visited[next_x] = visited[x] + 1
                 queue.append(next_x)
 
-                if next_x == k:
-                    return visited[next_x]
 print(bfs(n))
