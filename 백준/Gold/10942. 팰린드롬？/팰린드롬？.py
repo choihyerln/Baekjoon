a04e9941f3c00d74e046f628251117e09fb16572
Hyerin <116632714+choihyerln@users.noreply.github.com>
2023-08-26 22:32:55 +0900

    [Gold IV] Title: 팰린드롬?, Time: 944 ms, Memory: 174520 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -8,17 +8,17 @@ m = int(input())
 dp = [[0]*n for _ in range(n)]
 
 for i in range(n):
-    dp[i][i]=1
+    dp[i][i] = 1
 
 for i in range(n-1):
     if nums[i] == nums[i+1]:
-        dp[i][i+1]=1
+        dp[i][i+1] = 1
 
 for gap in range(2, n):
     for start in range(n-gap):
         if nums[start] == nums[start+gap]:
             if dp[start+1][start+gap-1] == 1:
-                dp[start][start+gap]=1
+                dp[start][start+gap] = 1
 
 for _ in range(m):
     s, e = map(int, input().split())
